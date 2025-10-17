3df5cf48d8fb6e278c0c25a63e2d2508535241a7
ShuvayuGoswami9551 <s_goswami@mail.fhsu.edu>
2025-10-17 00:55:29 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Imports
+import tkinter as tk
+
+# Window Setup
+window = tk.Tk()
+window.title("Wild West Poster Generator")
+window.geometry("1080x720")
+window.config(bg="orange2")
+
+# Window Customization
+mainframe = tk.Frame(window, width=1060, height=700)
+mainframe.pack(padx=10, pady=10)
+
+
+# Starts the window
+window.mainloop()
